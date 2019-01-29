dcbfd3c183747ddf81af4292f4c3d97c354a8ebb
Juank628 <jsanchez_zunino@yahoo.com>
2019-01-28 22:44:39 -0500

    total finished

--- src/components/TotalAndPay.js
+++ src/components/TotalAndPay.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Consumer } from "../context";
 
 export default class TotalAndPay extends Component {
   render() {
     return (
-      <div className="cFixed cBgWhite">
-        <span className="cTotalAndPayTitle">Total:</span><br />
-        <span className="cTotalAndPayPrice">S/28.99</span><br />
-        <button className="mt-2 mb-3 btn btn-success btn-lg btn-block"><span className="cTotalAndPayBtn">Pedir</span></button>
-      </div>
-    )
+      <Consumer>
+        {value => {
+          let totalPrice = 0;
+          for(let i=0;i<value.productArray.length;i++){
+            totalPrice=totalPrice+value.quantityArray[i]*value.priceArray[i];
+          }
+          return (
+            <div className="cFixed cBgWhite">
+              <span className="cTotalAndPayTitle">Total:</span>
+              <br />
+              <span className="cTotalAndPayPrice">S/{totalPrice}</span>
+              <br />
+              <button className="mt-2 mb-3 btn btn-success btn-lg btn-block">
+                <span className="cTotalAndPayBtn">Pedir</span>
+              </button>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
   }
 }
