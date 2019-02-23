9d6b18a77f83f86168d8a66caf4cc7e35f548467
Juank628 <jsanchez_zunino@yahoo.com>
2019-02-23 18:21:24 -0500

    img for cigaretes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -156,7 +156,7 @@ class Navbar extends Component {
                         itemClass="cNavBarCigarros"
                         onClick={value.apiQuery.bind(
                           this,
-                          "products",
+                          "/products",
                           "/cigarros",
                           ""
                         )}
